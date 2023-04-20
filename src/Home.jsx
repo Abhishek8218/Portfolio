6f01f5f7b4b68febc17bb00ccdc27d41802f6b6a
Abhishek8218 <116146568+Abhishek8218@users.noreply.github.com>
2023-04-20 02:36:19 +0000

    Changed the Title in home section

--- src/Home.jsx
+++ src/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
     <div name="home" className=" md:pt-20  h-fit w-full bg-gradient-to-b from-black to-gray-800 text-white md:h-fit">
       <div className=" max-w-screen-lg mx-auto flex flex-col items-center justify-center h-fit px-4 md:flex-row py-10">
         <div className="flex flex-col justify-center h-fit pt-20 pb-15 md:mr-8">
-          <h2 className="text-4xl sm:text-7xl font-bold">I'm a Frontend Developer</h2>
+          <h2 className="text-4xl sm:text-7xl font-bold"> Front-End React Developer</h2>
           <p className="mt-4  text-gray-300">
             Welcome to my portfolio website! I'm Abhishek, a frontend developer. I'm passionate about creating visually appealing and functional websites using the latest web technologies. Currently, I specialize in frontend development and am looking forward to learning fullstack development soon. Feel free to browse my portfolio and reach out to me with any questions or project inquiries. Thank you for stopping by!
           </p>
