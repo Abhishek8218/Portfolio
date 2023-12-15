7297617823934deeb4bac06d71d4554072eeabec
Abhishek8218 <116146568+Abhishek8218@users.noreply.github.com>
2023-12-15 09:39:52 +0000

    About update

--- src/About.jsx
+++ src/About.jsx
@@ -14,7 +14,7 @@ const About = () => {
         </div>
 
         <p className="text-xl mt-16">
-          I am currently pursuing my second year in BCA and have recently completed a 6-month web development course with React from Codeyogi platform. As an aspiring fullstack web developer, I am deeply passionate about technology and enjoy creating functional and visually appealing websites using the latest web technologies.
+          I am currently pursuing my third year in BCA and have recently completed a 6-month web development course with React from Codeyogi platform. As an aspiring fullstack web developer, I am deeply passionate about technology and enjoy creating functional and visually appealing websites using the latest web technologies.
         </p>
 
         <br />
