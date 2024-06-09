0ce2b380595b1b8da07cf7fa80b9089325358aa0
Abhishek Kumar <116146568+Abhishek8218@users.noreply.github.com>
2024-06-09 11:38:41 +0530

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -17,7 +17,7 @@ const Home = () => {
 
 
           <div>
-            <a href={Abhishek_Kumar_Resume} download >
+            <a href="#" download >
               <div className="flex py-3 px-5  my-6 items-center bg-gradient-to-r from-cyan-500 to-blue-500 rounded-md text-white 
  w-fit" >
                 Resume <span className=""><AiOutlineDownload size={20} className=" ml-1" /></span>
