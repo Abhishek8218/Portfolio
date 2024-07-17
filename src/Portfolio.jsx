8499143b6c588a5423a338d4d1c48eb43b32dc36
Abhishek Kumar <116146568+Abhishek8218@users.noreply.github.com>
2024-07-17 11:55:27 +0530

    Update Portfolio.jsx
    updated project link

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -68,7 +68,7 @@ const Portfolio = () => {
       src: academixPro,
       //src: "https://images.unsplash.com/photo-1593444285563-4479fb28226e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       title: "AcademixPro",
-      href: "https://infinity-insights.vercel.app/"
+      href: "https://academixpro.vercel.app/"
     }
      
   ];
